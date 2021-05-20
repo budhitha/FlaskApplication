9d5a04d0d60b16c21fc55638f940ebf1b3a1b4f7
Budhitha Perera <budhitha.lp@live.com>
2021-05-20 12:23:55 +0530

    Advanced Flask-JWT Configuration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from datetime import timedelta
+
+from flask import Flask, jsonify
 from flask_jwt import JWT
 from flask_restful import Api
 
@@ -10,7 +12,28 @@ app = Flask(__name__)
 app.secret_key = 'budhitha'
 api = Api(app)
 
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+# config JWT to expire within half an hour
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+# config JWT auth key name to be 'email' instead of default 'username'
+app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
 jwt = JWT(app, authenticate, identity)
+
+@jwt.auth_response_handler
+def customized_response_handler(access_token, identity):
+    return jsonify({
+                        'access_token': access_token.decode('utf-8'),
+                        'user_id': identity.id
+                   })
+
+@jwt.jwt_error_handler
+def customized_error_handler(error):
+    return jsonify({
+                       'message': error.description,
+                       'code': error.status_code
+                   }), error.status_code
+
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
