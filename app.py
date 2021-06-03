812d0939bbbc9b55d2d32d339de641b3cc21448a
Budhitha Perera <budhitha.lp@live.com>
2021-06-03 11:45:41 +0530

    JWT errors configured

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from resources.user import UserRegister, User, UserLogin
+from resources.user import UserRegister, User, UserLogin, TokenRefresh
 
 uri = os.getenv("DATABASE_URL", 'sqlite:///storeData.db')
 if uri.startswith("postgres://"):
@@ -21,12 +21,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'budhitha'  # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
-# app.config['JWT_AUTH_URL_RULE'] = '/login'
-# # config JWT to expire within half an hour
-# app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
-# # config JWT auth key name to be 'email' instead of default 'username'
-# app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
-
 jwt = JWTManager(app)
 
 
@@ -37,20 +31,44 @@ def add_claims_to_jwt(identity):
     return {'is_admin': False}
 
 
-# @jwt.auth_response_handler
-# def customized_response_handler(access_token, identity):
-#     return jsonify({
-#         'access_token': access_token.decode('utf-8'),
-#         'user_id': identity.id
-#     })
-#
-#
-# @jwt.jwt_error_handler
-# def customized_error_handler(error):
-#     return jsonify({
-#         'message': error.description,
-#         'code': error.status_code
-#     }), error.status_code
+@jwt.expired_token_loader
+def expired_token_callback():
+    return jsonify({
+        'description': 'The token has expired.',
+        'error': 'token_expired'
+    }), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return jsonify({
+        'description': 'Signature verification failed.',
+        'error': 'invalid_token'
+    }), 401
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return jsonify({
+        'description': 'Request does not contain an access token.',
+        'error': 'authorization_required'
+    }), 401
+
+
+@jwt.needs_fresh_token_loader
+def token_not_fresh_callback():
+    return jsonify({
+        'description': 'The token is not fresh.',
+        'error': 'fresh_token_required'
+    }), 401
+
+
+@jwt.revoked_token_loader
+def revoked_token_callback():
+    return jsonify({
+        'description': 'The token has been revoked',
+        'error': 'token_revoked'
+    }), 401
 
 
 api.add_resource(Store, '/store/<string:name>')
@@ -60,6 +78,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
+api.add_resource(TokenRefresh, '/refresh')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -1,4 +1,5 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (create_access_token, create_refresh_token,
+                                jwt_required, get_jwt_identity)
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
@@ -55,3 +56,11 @@ class UserLogin(Resource):
             return {'access_token': access_token, 'refresh_token': refresh_token}
 
         return {'message': 'Invalid credentials'}, 401
+
+
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}
